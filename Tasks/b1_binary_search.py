63b84720e5d760429477b77c82f05f2cb227d2a0
Evgeniy Gaydarenko <62751528+Gaydar-Kanub@users.noreply.github.com>
2020-05-28 19:25:37 +0300

    b1 done

--- Tasks/b1_binary_search.py
+++ Tasks/b1_binary_search.py
@@ -10,4 +10,20 @@ def binary_search(elem: int, arr: Sequence) -> Optional[int]:
     :return: Index of element if it's presented in the arr, None otherwise
     """
     print(elem, arr)
-    return None
+    if elem not in arr:
+        return None
+    start = 0
+    finish = len(arr)
+    while True:
+        half = int((finish + start) // 2)
+        if half == start:
+            return start
+        if elem in arr[start: half]:
+            finish = half
+        else:
+            start = half
+
+
+if __name__ == '__main__':
+    x = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    print(binary_search(5, x))
