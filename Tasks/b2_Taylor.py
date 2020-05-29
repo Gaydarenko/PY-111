20344d89615f50852cc6d50e672102839b11518e
Evgeniy Gaydarenko <62751528+Gaydar-Kanub@users.noreply.github.com>
2020-05-29 11:40:38 +0300

    b2 release sinx(x)

--- Tasks/b2_Taylor.py
+++ Tasks/b2_Taylor.py
@@ -2,7 +2,9 @@
 Taylor series
 """
 from typing import Union
-
+from numpy import e
+from numpy import log
+from numpy import sin
 
 def ex(x: Union[int, float]) -> float:
     """
@@ -23,4 +25,16 @@ def sinx(x: Union[int, float]) -> float:
     :return: sin(x) value
     """
     print(x)
-    return 0
+    res = 0
+    for n in range(1, 7):
+        fact_n = 1
+        for i in range(1, 2 * n):
+            fact_n *= i
+        res += (-1) ** (n - 1) * x ** (2 * n - 1) / fact_n
+    return res
+
+
+# if __name__ == '__main__':
+    # print(e)
+    print(sinx(1.55433))
+    print(sin(1.55433))
