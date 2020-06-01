211d87b2807128d506e30b501f9f52f5aa0c802c
Evgeniy Gaydarenko <62751528+Gaydar-Kanub@users.noreply.github.com>
2020-06-01 12:03:11 +0300

    c2 done Спасибо за подсказку. Было еще одно похожее "узкое горлышко". Всё исправил.

--- Tasks/c2_recursive_binary_search.py
+++ Tasks/c2_recursive_binary_search.py
@@ -10,13 +10,19 @@ def binary_search(elem: int, arr: Sequence) -> Optional[int]:
     :return: Index of element if it's presented in the arr, None otherwise
     """
     print(elem, arr)
-    if elem not in arr:
-        return None
     try:
         half = (binary_search.finish + binary_search.start) // 2
         if half == binary_search.start:
-            return binary_search.start
-        if elem in arr[binary_search.start: half]:
+            res = binary_search.start
+            del (binary_search.start, binary_search.finish)
+            if elem == arr[half]:
+                return res
+            else:
+                return None
+        if elem == arr[half-1] and elem != arr[half-2]:
+            del (binary_search.start, binary_search.finish)
+            return half - 1
+        if elem < arr[half]:
             binary_search.finish = half
         else:
             binary_search.start = half
@@ -29,7 +35,8 @@ def binary_search(elem: int, arr: Sequence) -> Optional[int]:
 if __name__ == '__main__':
     # x = [0, 1, 2, 3, 4, 5, 6, 7, 9]
     # print(binary_search(9, x))
-    y = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 101]
-    y = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 101]
-    print(binary_search(101, y))
-    print(y.index(101))
+    # y = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 101]
+    # y = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 101]
+    # print(binary_search(101, y))
+    # print(y.index(101))
+    print(binary_search(2, [1, 2, 2, 2]))
