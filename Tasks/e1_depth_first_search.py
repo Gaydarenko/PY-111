06085e4f1df2cac83229cda6d61a64f2f22254c5
Evgeniy Gaydarenko <62751528+Gaydar-Kanub@users.noreply.github.com>
2020-06-06 12:49:15 +0300

    e1 done - test passed successful

--- Tasks/e1_depth_first_search.py
+++ Tasks/e1_depth_first_search.py
@@ -1,5 +1,6 @@
 from typing import Hashable, List
 import networkx as nx
+import matplotlib.pyplot as plt
 
 
 def dfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
@@ -11,4 +12,31 @@ def dfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
     :return: list of nodes in the visited order
     """
     print(g, start_node)
-    return list(g.nodes)
+    queue = [start_node, ]
+    viewed = [start_node, ]
+    res = []
+    while len(queue):
+        n = queue.pop()
+        res.append(n)
+        for neighbor in g.neighbors(n):
+            if neighbor not in viewed:
+                queue.append(neighbor)
+                viewed.append(neighbor)
+    return res
+
+
+if __name__ == '__main__':
+    graph = nx.Graph()
+    graph.add_nodes_from("ABCDEFG")
+    graph.add_edges_from([
+        ('A', 'B'),
+        ('A', 'C'),
+        ('B', 'D'),
+        ('B', 'E'),
+        ('C', 'F'),
+        ('E', 'G'),
+    ])
+    # nx.draw(graph, with_labels=True)
+    # plt.show()
+    print('!')
+    dfs(graph, 'A')
