efa63883593fd7d6a7a3cefc08c3573002148e21
Evgeniy Gaydarenko <62751528+Gaydar-Kanub@users.noreply.github.com>
2020-06-08 17:06:20 +0300

    add "def add step" d1 done - test passed successful

--- Tasks/d1_horse.py
+++ Tasks/d1_horse.py
@@ -9,34 +9,23 @@ def calculate_paths(shape: (int, int), point: (int, int)) -> int:
     print(shape, point)
     steps = [(0, 0), ]
     step_count = {(0, 0): 1}
+    def add_step(x, y, p):
+        try:
+            step_count[(x, y)] += 2 * step_count[p]
+        except:
+            steps.append((x, y))
+            step_count[(x, y)] = 2 * step_count[p]
     while steps:
         steps.sort()
         st = steps.pop(0)
         if st[0] + 1 < shape[0] and st[1] + 2 < shape[1]:
-            if (st[0] + 1, st[1] + 2) not in steps:
-                steps.append((st[0] + 1, st[1] + 2))
-                step_count[(st[0] + 1, st[1] + 2)] = 2 * step_count[(st[0], st[1])]
-            else:
-                step_count[(st[0] + 1, st[1] + 2)] += 2 * step_count[(st[0], st[1])]
+            add_step(st[0] + 1, st[1] + 2, (st[0], st[1]))
         if st[0] + 1 < shape[0] and st[1] - 2 >= 0:
-            if (st[0] + 1, st[1] - 2) not in steps:
-                steps.append((st[0] + 1, st[1] - 2))
-                step_count[(st[0] + 1, st[1] - 2)] = 2 * step_count[(st[0], st[1])]
-            else:
-                step_count[(st[0] + 1, st[1] - 2)] += 2 * step_count[(st[0], st[1])]
+            add_step(st[0] + 1, st[1] - 2, (st[0], st[1]))
         if st[0] + 2 < shape[0] and st[1] + 1 < shape[1]:
-            if (st[0] + 2, st[1] + 1) not in steps:
-                steps.append((st[0] + 2, st[1] + 1))
-                step_count[(st[0] + 2, st[1] + 1)] = 2 * step_count[(st[0], st[1])]
-            else:
-                step_count[(st[0] + 2, st[1] + 1)] += 2 * step_count[(st[0], st[1])]
+            add_step(st[0] + 2, st[1] + 1, (st[0], st[1]))
         if st[0] + 2 < shape[0] and st[1] - 1 >= 0:
-            if (st[0] + 2, st[1] - 1) not in steps:
-                steps.append((st[0] + 2, st[1] - 1))
-                step_count[(st[0] + 2, st[1] - 1)] = 2 * step_count[(st[0], st[1])]
-            else:
-                step_count[(st[0] + 2, st[1] - 1)] += 2 * step_count[(st[0], st[1])]
-    # print(step_count)
+            add_step(st[0] + 2, st[1] - 1, (st[0], st[1]))
     return step_count[point]
 
 
