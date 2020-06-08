65525ffb80cd0abcc3eda3a2f7b06d861a02e9ea
Evgeniy Gaydarenko <62751528+Gaydar-Kanub@users.noreply.github.com>
2020-06-08 13:59:26 +0300

    d1 done - test passed successful

--- Tasks/d1_horse.py
+++ Tasks/d1_horse.py
@@ -8,17 +8,38 @@ def calculate_paths(shape: (int, int), point: (int, int)) -> int:
     """
     print(shape, point)
     steps = [(0, 0), ]
-    for st in steps:
+    step_count = {(0, 0): 1}
+    while steps:
+        steps.sort()
+        st = steps.pop(0)
         if st[0] + 1 < shape[0] and st[1] + 2 < shape[1]:
-            steps.append((st[0] + 1, st[1] + 2))
+            if (st[0] + 1, st[1] + 2) not in steps:
+                steps.append((st[0] + 1, st[1] + 2))
+                step_count[(st[0] + 1, st[1] + 2)] = 2 * step_count[(st[0], st[1])]
+            else:
+                step_count[(st[0] + 1, st[1] + 2)] += 2 * step_count[(st[0], st[1])]
+        if st[0] + 1 < shape[0] and st[1] - 2 >= 0:
+            if (st[0] + 1, st[1] - 2) not in steps:
+                steps.append((st[0] + 1, st[1] - 2))
+                step_count[(st[0] + 1, st[1] - 2)] = 2 * step_count[(st[0], st[1])]
+            else:
+                step_count[(st[0] + 1, st[1] - 2)] += 2 * step_count[(st[0], st[1])]
         if st[0] + 2 < shape[0] and st[1] + 1 < shape[1]:
-            steps.append((st[0] + 2, st[1] + 1))
-        if st[0] - 1 >= 0 and st[1] + 2 < shape[1]:
-            steps.append((st[0] - 1, st[1] + 2))
-        if st[1] - 1 >= 0 and st[1] + 1 < shape[1]:
-            steps.append((st[0] + 2, st[1] - 1))
-    return steps
+            if (st[0] + 2, st[1] + 1) not in steps:
+                steps.append((st[0] + 2, st[1] + 1))
+                step_count[(st[0] + 2, st[1] + 1)] = 2 * step_count[(st[0], st[1])]
+            else:
+                step_count[(st[0] + 2, st[1] + 1)] += 2 * step_count[(st[0], st[1])]
+        if st[0] + 2 < shape[0] and st[1] - 1 >= 0:
+            if (st[0] + 2, st[1] - 1) not in steps:
+                steps.append((st[0] + 2, st[1] - 1))
+                step_count[(st[0] + 2, st[1] - 1)] = 2 * step_count[(st[0], st[1])]
+            else:
+                step_count[(st[0] + 2, st[1] - 1)] += 2 * step_count[(st[0], st[1])]
+    # print(step_count)
+    return step_count[point]
 
 
 if __name__ == '__main__':
     print(calculate_paths((8, 8), (7, 7)))
+    print(calculate_paths((9, 9), (8, 8)))
