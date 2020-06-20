c9a6ae2dbe1c29e0a4c74f7553cc507e0659b36b
Evgeniy Gaydarenko <62751528+Gaydar-Kanub@users.noreply.github.com>
2020-06-20 17:19:01 +0300

    "def remove()" release add comments to "def insert()"
    All is implemented in dictionaries

--- Tasks/f0_binary_search_tree.py
+++ Tasks/f0_binary_search_tree.py
@@ -19,22 +19,30 @@ def insert(key: int, value: Any) -> None:
     :return: None
     """
     global tree
-    print(key, value)
+    # print(key, value)
     # graph = nx.Graph()
     # if not graph.nodes:
     #     graph.add_node(key)
 
+    # Создаю функцию для рекурсивной обработки дерева
     def my_insert(my_tree):
+        # Если дерево/поддерево пустое добавляем узел
         if not my_tree:
             return my_tree.update({'key': key, 'value': value})
+        # Если ключ больше корневого даноого дерева/поддерева, то...
         elif key > my_tree['key']:
+            # Если правый потомок отсутствует, то создать его с ключом и значением
             if my_tree.get('right') is None:
                 return my_tree.update({'right': {'key': key, 'value': value}})
+            # Если правый потомок уже имеется, перейти в правую ветку и запустить функцию снова
             else:
                 return my_insert(my_tree['right'])
+        # Если ключ меньше корневого даноого дерева/поддерева, то...
         elif key < my_tree['key']:
+            # Если левый потомок отсутствует, то создать его с ключом и значением
             if my_tree.get('left') is None:
                 return my_tree.update({'left': {'key': key, 'value': value}})
+            # Если левый потомок уже имеется, перейти в правую ветку и запустить функцию снова
             else:
                 return my_insert(my_tree['left'])
     return my_insert(tree)
@@ -48,7 +56,45 @@ def remove(key: int) -> Optional[Tuple[int, Any]]:
     :return: deleted (key, value) pair or None
     """
     print(key)
-    return None
+    global tree
+
+    def my_remove(my_tree):
+        if key > my_tree['key']:
+            return my_remove(my_tree['right'])
+        elif key < my_tree['key']:
+            return my_remove(my_tree['left'])
+        else:
+            my_remove.pair = tuple(my_tree.values())
+            if my_tree.get('right') is None and my_tree.get('left') is None:
+                return my_tree.clear()
+            elif my_tree.get('right') is None:
+                return my_tree.update(my_tree.pop('left'))
+            elif my_tree.get('left') is None:
+                return my_tree.update(my_tree.pop('right'))
+            else:
+                find_min_in_right(my_tree['right'])
+                my_tree.update(find_min_in_right.temp)
+
+    def find_min_in_right(small_tree):
+        if small_tree.get('left') is not None:
+            if len(small_tree['left'].keys()) > 2:
+                return find_min_in_right(small_tree['left'])
+            else:
+                find_min_in_right.temp = small_tree['left']
+                del small_tree['left']
+                return small_tree
+        elif small_tree.get('right') is not None:
+            if len(small_tree['right'].keys()) > 2:
+                return find_min_in_right(small_tree['right'])
+            else:
+                find_min_in_right.temp = small_tree['right']
+                del small_tree['right']
+                return small_tree
+        else:
+            find_min_in_right.temp = small_tree.copy()
+            return small_tree.clear()
+    my_remove(tree)
+    return my_remove.pair
 
 
 def find(key: int) -> Optional[Any]:
@@ -93,8 +139,14 @@ if __name__ == '__main__':
     insert(42, 'The meaning of life, the universe and everything.')
     insert(0, 'ZERO!')
     insert(13, "Devil's sign here")
-    insert(13, "Oh no, devil's sign again Oo")
+    insert(53, "Oh no, devil's sign again Oo")
+    insert(47, "forty seven")
+    insert(80, "eighty")
     print(json.dumps(tree, indent='\t'))
-    print(find(42))
-    print(find(13))
-    print(find(-999))
+    # print(find(42))
+    # print(find(13))
+    # print(find(-999))
+    print('Function remove start')
+    print(remove(42))
+    print('Function remove finish')
+    print(json.dumps(tree, indent='\t'))
